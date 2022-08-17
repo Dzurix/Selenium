f6700679f1f52124fda811f5f4f045597ef415c8
Dejan Stanišin <deki2ns@gmail.com>
2022-08-17 07:16:54 +0200

    frames

--- intro/demo/src/test/java/intro/actionsDemo.java
+++ intro/demo/src/test/java/intro/actionsDemo.java
@@ -1,7 +1,11 @@
 package intro;
 
+import java.util.Iterator;
+import java.util.Set;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
@@ -10,18 +14,81 @@ public class actionsDemo {
   public static void main(String[] args) {
     System.setProperty(
       "webdriver.chrome.driver",
-      //  "C:/Users/deki2/OneDrive/Documents/chromedriver_win32/chromedriver.exe" // LAPTOP
-      "C:/Users/dst/Documents/chromedriver_win32/chromedriver.exe" //POSO
+      "C:/Users/deki2/OneDrive/Documents/chromedriver_win32/chromedriver.exe" // LAPTOP
+      // "C:/Users/dst/Documents/chromedriver_win32/chromedriver.exe" //POSO
     );
     WebDriver driver = new ChromeDriver();
+    driver.manage().window().maximize(); //maximiziranje prozora
+    // driver.get("https://www.amazon.com/");
+    // Actions a = new Actions(driver); // definisanje klase ACTIONS
 
-    driver.get("https://www.amazon.com/");
-    Actions a = new Actions(driver); // definisanje klase ACTIONS
-    a
-      .moveToElement(
-        driver.findElement(By.cssSelector("div[id='nav-flyout-accountList']"))
-      )
-      .build()
-      .perform();
+    // // move to specific element
+    // a
+    //   .moveToElement(
+    //     driver.findElement(By.cssSelector("a[id='nav-link-accountList']"))
+    //   )
+    //   .contextClick()
+    //   .build()
+    //   .perform(); // sintaksa Actions klase
+
+    // // koristim velika slova
+
+    // a
+    //   .moveToElement(
+    //     driver.findElement(By.cssSelector("input[id='twotabsearchtextbox']"))
+    //   )
+    //   .click()
+    //   .keyDown(Keys.SHIFT)
+    //   .sendKeys("hello")
+    //   .doubleClick()
+    //   .build()
+    //   .perform();
+
+    //CHILD WINDOWS vezba
+
+    // driver.get("https://rahulshettyacademy.com/loginpagePractise/");
+
+    // driver.findElement(By.cssSelector("a[class='blinkingText']")).click();
+    // Set<String> numOfWindows = driver.getWindowHandles(); // sad su dodeljeni ID od aktivnih prozora
+    // Iterator<String> windowID = numOfWindows.iterator(); // skladistim ID od parenta i child
+
+    // String parentID = windowID.next(); //skladistim ID od parenta
+    // String childID = windowID.next(); // kada ga opet pozovem, prelazim na CHILD prozor
+
+    // driver.switchTo().window(childID);
+
+    // System.out.println(
+    //   driver
+    //     .findElement(By.cssSelector(".im-para.red"))
+    //     .getText()
+    //     .split("at")[1].trim()
+    //     .split(" ")[0]
+    // );
+    // // izvlacim samo naziv email
+    // String emailID = driver
+    //   .findElement(By.cssSelector(".im-para.red"))
+    //   .getText()
+    //   .split("at")[1].trim()
+    //   .split(" ")[0];
+
+    // driver.switchTo().window(parentID); //prebacujem se na GLAVNI prozor
+
+    // driver.findElement(By.id("username")).sendKeys(emailID);
+
+    //FRAMES vezba
+
+    driver.get("https://jqueryui.com/droppable/");
+    driver
+      .switchTo()
+      .frame(driver.findElement(By.cssSelector("iframe[class='demo-frame']")));
+    System.out.println(driver.findElements(By.tagName("iframe")).size()); //da vidim koliko ima frejmova
+
+    Actions a = new Actions(driver);
+    WebElement source = driver.findElement(By.id("draggable"));
+    WebElement target = driver.findElement(By.id("droppable"));
+
+    a.dragAndDrop(source, target).build().perform();
+
+    driver.switchTo().defaultContent(); //ovako izlazim iz frejma
   }
 }
--- intro/demo/src/test/java/intro/alerts.java
+++ intro/demo/src/test/java/intro/alerts.java
@@ -1,7 +1,9 @@
 package intro;
 
 import java.time.Duration;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 import javax.naming.directory.DirContext;
 import org.apache.commons.exec.ProcessDestroyer;
 import org.openqa.selenium.By;
@@ -18,8 +20,8 @@ public class alerts {
   public static void main(String[] args) throws InterruptedException {
     System.setProperty(
       "webdriver.chrome.driver",
-      //  "C:/Users/deki2/OneDrive/Documents/chromedriver_win32/chromedriver.exe" // LAPTOP
-      "C:/Users/dst/Documents/chromedriver_win32/chromedriver.exe" //POSO
+      "C:/Users/deki2/OneDrive/Documents/chromedriver_win32/chromedriver.exe" // LAPTOP
+      // "C:/Users/dst/Documents/chromedriver_win32/chromedriver.exe" //POSO
     );
     WebDriver driver = new ChromeDriver(); //uvek pisem WebDriver
     // String ime = "Upoje";
@@ -86,63 +88,97 @@ public class alerts {
     // );
     // ASIGNMENT 3
 
-    WebDriverWait w = new WebDriverWait(driver, Duration.ofSeconds(5));
+    //   WebDriverWait w = new WebDriverWait(driver, Duration.ofSeconds(5));
 
-    driver.get("https://rahulshettyacademy.com/loginpagePractise/");
+    //   driver.get("https://rahulshettyacademy.com/loginpagePractise/");
+
+    //   driver
+    //     .findElement(By.cssSelector("#username"))
+    //     .sendKeys("rahulshettyacademy");
+
+    //   driver.findElement(By.cssSelector("#password")).sendKeys("learning");
+    //   driver.findElement(By.xpath("//label[2]/span[2]")).click();
+
+    //   w.until(
+    //     ExpectedConditions.visibilityOfElementLocated(
+    //       By.cssSelector("div.modal-content")
+    //     )
+    //   );
+
+    //   driver.findElement(By.cssSelector("#okayBtn")).click();
+
+    //   WebElement staticDropdown = driver.findElement(
+    //     By.cssSelector("select.form-control")
+    //   ); //hvatamo dropdown listu
+
+    //   Select dropdown = new Select(staticDropdown); //kreirali smo objekat od te klase
+
+    //   dropdown.selectByValue("consult"); //selektovanje opcije "Consultant"
+
+    //   driver.findElement(By.id("terms")).click(); //Accept terms
+
+    //   driver.findElement(By.id("signInBtn")).click(); // sign in button
+
+    //   //secekamo da se stranica ucita
+    //   w.until(
+    //     ExpectedConditions.urlMatches(
+    //       "https://rahulshettyacademy.com/angularpractice/shop"
+    //     )
+    //   );
+
+    //   //kupujem proizvod
+
+    //   buyProduct(driver);
+
+    //   driver
+    //     .findElement(By.cssSelector("a[class='nav-link btn btn-primary']"))
+    //     .click();
+    // }
+
+    // //kreiranje metoda izvan MAIN-a
+
+    // public static void buyProduct(WebDriver driver) {
+    //   List<WebElement> products = driver.findElements(
+    //     // List<WebElement> pisem posto ima vise od jednog elementa (inace bi bio samo jedan WebElement)
+    //     By.cssSelector("div.card-footer button")
+    //   );
+
+    //   for (int i = 0; i < products.size(); i++) {
+    //     products.get(i).click();
+    //   }
+
+    //ASIGNMENT 4
+
+    // driver.get("https://the-internet.herokuapp.com/");
+
+    // driver.findElement(By.cssSelector("a[href='/windows']")).click();
+    // driver.findElement(By.cssSelector("a[href='/windows/new']")).click();
+
+    // Set<String> numOfWindows = driver.getWindowHandles(); // sad su dodeljeni ID od aktivnih prozora
+    // Iterator<String> windowID = numOfWindows.iterator(); // skladistim ID od parenta i child
+
+    // String parentID = windowID.next(); //skladistim ID od parenta
+    // String childID = windowID.next(); // kada ga opet pozovem, prelazim na CHILD prozor
+
+    // driver.switchTo().window(childID);
+
+    // String childText = driver.findElement(By.tagName("h3")).getText();
+    // System.out.println(childText);
+
+    // driver.switchTo().window(parentID);
+    // String parentText = driver.findElement(By.tagName("h3")).getText();
+    // System.out.println(parentText);
+
+    //ASSIGNMENT 5
+
+    driver.get("https://the-internet.herokuapp.com/");
+
+    driver.findElement(By.linkText("Nested Frames")).click();
 
     driver
-      .findElement(By.cssSelector("#username"))
-      .sendKeys("rahulshettyacademy");
+      .switchTo()
+      .frame(driver.findElement(By.cssSelector("frame[name='frame-middle']")));
 
-    driver.findElement(By.cssSelector("#password")).sendKeys("learning");
-    driver.findElement(By.xpath("//label[2]/span[2]")).click();
-
-    w.until(
-      ExpectedConditions.visibilityOfElementLocated(
-        By.cssSelector("div.modal-content")
-      )
-    );
-
-    driver.findElement(By.cssSelector("#okayBtn")).click();
-
-    WebElement staticDropdown = driver.findElement(
-      By.cssSelector("select.form-control")
-    ); //hvatamo dropdown listu
-
-    Select dropdown = new Select(staticDropdown); //kreirali smo objekat od te klase
-
-    dropdown.selectByValue("consult"); //selektovanje opcije "Consultant"
-
-    driver.findElement(By.id("terms")).click(); //Accept terms
-
-    driver.findElement(By.id("signInBtn")).click(); // sign in button
-
-    //secekamo da se stranica ucita
-    w.until(
-      ExpectedConditions.urlMatches(
-        "https://rahulshettyacademy.com/angularpractice/shop"
-      )
-    );
-
-    //kupujem proizvod
-
-    buyProduct(driver);
-
-    driver
-      .findElement(By.cssSelector("a[class='nav-link btn btn-primary']"))
-      .click();
-  }
-
-  //kreiranje metoda izvan MAIN-a
-
-  public static void buyProduct(WebDriver driver) {
-    List<WebElement> products = driver.findElements(
-      // List<WebElement> pisem posto ima vise od jednog elementa (inace bi bio samo jedan WebElement)
-      By.cssSelector("div.card-footer button")
-    );
-
-    for (int i = 0; i < products.size(); i++) {
-      products.get(i).click();
-    }
+    System.out.println(driver.findElement(By.id("content")).getText());
   }
 }
