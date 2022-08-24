dd1b95057183067bfeb7f72ee07faefc1f615566
Dexter <deki2ns@gmail.com>
2022-08-24 15:33:35 +0200

    calendars

--- intro/demo/src/test/java/intro/actionsDemo.java
+++ intro/demo/src/test/java/intro/actionsDemo.java
@@ -1,6 +1,7 @@
 package intro;
 
 import java.util.Iterator;
+import java.util.List;
 import java.util.Set;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
@@ -93,43 +94,79 @@ public class actionsDemo {
 
     // HOW TO PRINT THE LINKS COUNT IN THE PAGE
 
-    driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+    // driver.get("https://rahulshettyacademy.com/AutomationPractice/");
 
-    System.out.println(driver.findElements(By.tagName("a")).size()); //kako da pronadjem linkove preko 'a' href
+    // System.out.println(driver.findElements(By.tagName("a")).size()); //kako da pronadjem linkove preko 'a' href
 
-    // broj linkova u footeru - treba da smanjimo WebDriver SCOPE, da bi samo footer pregledao
+    // // broj linkova u footeru - treba da smanjimo WebDriver SCOPE, da bi samo footer pregledao
 
-    WebElement footerDriver = driver.findElement(By.id("gf-BIG")); //smanjivanje SCOPA preko WebElementa
+    // WebElement footerDriver = driver.findElement(By.id("gf-BIG")); //smanjivanje SCOPA preko WebElementa
 
-    System.out.println(footerDriver.findElements(By.tagName("a")).size());
+    // System.out.println(footerDriver.findElements(By.tagName("a")).size());
 
-    WebElement columnDriver = footerDriver.findElement(
-      By.xpath("//table/tbody/tr/td[1]/ul")
-    ); // da pronadjem jos manji SCOPE
+    // WebElement columnDriver = footerDriver.findElement(
+    //   By.xpath("//table/tbody/tr/td[1]/ul")
+    // ); // da pronadjem jos manji SCOPE
 
-    System.out.println(columnDriver.findElements(By.tagName("a")).size());
-    // how to click on each link in the column adn check if the pages are opening
+    // System.out.println(columnDriver.findElements(By.tagName("a")).size());
+    // // how to click on each link in the column adn check if the pages are opening
 
-    for (
-      int i = 1;
-      i < columnDriver.findElements(By.tagName("a")).size();
-      i++
+    // for (
+    //   int i = 1;
+    //   i < columnDriver.findElements(By.tagName("a")).size();
+    //   i++
+    // ) {
+    //   String clickOnLinkTab = Keys.chord(Keys.CONTROL, Keys.ENTER); // klik na tockic na misu (kao da je klikno)
+
+    //   columnDriver
+    //     .findElements(By.tagName("a"))
+    //     .get(i)
+    //     .sendKeys(clickOnLinkTab);
+    // } // sa ovim FOR loopom otvaramo sve tabove (klikcemo na linkove)
+    // Thread.sleep(5000);
+
+    // Set<String> abc = driver.getWindowHandles();
+    // Iterator<String> it = abc.iterator(); // iterator sluzi da menjamo tabove u browseru
+
+    // while (it.hasNext()) { // dok god ima sledeci, predji na taj sledeci
+    //   driver.switchTo().window(it.next());
+    //   System.out.println(driver.getTitle());
+    // }
+
+    // WORKING WITH CALENDARS
+
+    driver.get(
+      "http://seleniumpractise.blogspot.com/2016/08/how-to-handle-calendar-in-selenium.html"
+    );
+    // Avgust 27
+    driver.findElement(By.id("datepicker")).click();
+
+    // kako da klikce dugme dok ne bude zeljeni mesec preko WHILE LOOP-a
+    while (
+      !driver
+        .findElement(By.className("ui-datepicker-month"))
+        .getText()
+        .contains("April")
     ) {
-      String clickOnLinkTab = Keys.chord(Keys.CONTROL, Keys.ENTER); // klik na tockic na misu (kao da je klikno)
+      driver.findElement(By.cssSelector(".ui-datepicker-next")).click();
+    }
+    List<WebElement> dates = driver.findElements(
+      By.className("ui-state-default")
+    );
 
-      columnDriver
-        .findElements(By.tagName("a"))
+    //Grab common attribute => Put inot list => Iterate through it
+    int count = driver.findElements(By.className("ui-state-default")).size();
+
+    for (int i = 0; i < count; i++) {
+      String text = driver
+        .findElements(By.className("ui-state-default"))
         .get(i)
-        .sendKeys(clickOnLinkTab);
-    } // sa ovim FOR loopom otvaramo sve tabove (klikcemo na linkove)
-    Thread.sleep(5000);
+        .getText();
 
-    Set<String> abc = driver.getWindowHandles();
-    Iterator<String> it = abc.iterator(); // iterator sluzi da menjamo tabove u browseru
-
-    while (it.hasNext()) { // dok god ima sledeci, predji na taj sledeci
-      driver.switchTo().window(it.next());
-      System.out.println(driver.getTitle());
+      if (text.equalsIgnoreCase("23")) {
+        driver.findElements(By.className("ui-state-default")).get(i).click();
+        break;
+      }
     }
   }
 }
