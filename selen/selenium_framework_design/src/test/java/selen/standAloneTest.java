ccdbf76923e1c95f337ed65b9682c6a727228910
Dejan Stanišin <deki2ns@gmail.com>
2022-09-02 05:37:09 +0200

    end2end testing

--- selen/selenium_framework_design/src/test/java/selen/standAloneTest.java
+++ selen/selenium_framework_design/src/test/java/selen/standAloneTest.java
@@ -4,18 +4,23 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import java.time.Duration;
 import java.util.List;
 import java.util.stream.Collectors;
+import javax.swing.Action;
+import org.apache.hc.core5.util.Asserts;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 
 public class standAloneTest {
 
   public static void main(String[] args) {
-    WebDriverManager.chromedriver().setup();
+    String productName = "ZARA COAT 3";
+    WebDriverManager.chromedriver().setup(); //umemsto lokacije u pom.XML importujemo WebDriverManager
 
     WebDriver driver = new ChromeDriver();
     driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
@@ -25,6 +30,14 @@ public class standAloneTest {
     driver.findElement(By.id("userPassword")).sendKeys("Iamking@000");
     driver.findElement(By.id("login")).click();
 
+    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+
+    wait.until(
+      ExpectedConditions.visibilityOfElementLocated(
+        By.cssSelector(".card-body")
+      )
+    );
+
     List<WebElement> products = driver.findElements(
       By.cssSelector(".card-body")
     );
@@ -33,16 +46,69 @@ public class standAloneTest {
       .stream()
       .filter(
         product ->
-          product.findElement(By.tagName("b")).getText().equals("ZARA COAT 3")
+          product.findElement(By.tagName("b")).getText().equals(productName)
       )
       .findFirst()
       .orElse(null);
 
     prod.findElement(By.cssSelector(".card-body button:last-of-type")).click(); //dodavanje proizvoda u korpu
     //cekamo da se pojavi poruka da smo ubacili proizvod u korpu
-    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+
     wait.until(
       ExpectedConditions.visibilityOfElementLocated(By.id("toast-container"))
+    );
+
+    //cekamo da se skloni ona animacija nakon narucivanja proizvoda
+    wait.until(
+      ExpectedConditions.invisibilityOf(
+        driver.findElement(By.cssSelector(".ng-animating"))
+      )
+    ); //kad koristimo INVISIBILITY, onda treba cela putanja sa driver.....
+
+    driver
+      .findElement(By.cssSelector("[routerlink='/dashboard/cart']"))
+      .click();
+
+    //Provera sta imamo u korpi
+
+    List<WebElement> listaKorpa = driver.findElements(
+      By.cssSelector(".cartSection h3")
+    );
+
+    Boolean poklapanje = listaKorpa
+      .stream()
+      .anyMatch(p -> p.getText().equalsIgnoreCase(productName));
+
+    Assert.assertTrue(poklapanje);
+
+    // Odlazak na checkout
+    driver.findElement(By.cssSelector(".totalRow button")).click();
+
+    Actions a = new Actions(driver);
+
+    a
+      .sendKeys(
+        driver.findElement(By.cssSelector("[placeholder = 'Select Country']")),
+        "Yugo"
+      )
+      .build()
+      .perform();
+
+    wait.until(
+      ExpectedConditions.visibilityOfElementLocated(
+        By.cssSelector(".ta-results")
+      )
+    );
+
+    driver.findElement(By.cssSelector(".ta-item:nth-of-type(2)")).click();
+    driver.findElement(By.cssSelector("action__submit")).click();
+
+    String confirmMessage = driver
+      .findElement(By.cssSelector(".hero-primary"))
+      .getText();
+
+    Assert.assertTrue(
+      confirmMessage.equalsIgnoreCase("THANKYOU FOR THE ORDER.")
     );
   }
 }
