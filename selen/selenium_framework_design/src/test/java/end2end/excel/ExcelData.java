4479b3326db888d1dd5eda9d1257fecdca4674fe
Dejan Stanisin <deki2ns@gmail.com>
2022-09-27 06:43:20 +0200

    lol

--- selen/selenium_framework_design/src/test/java/end2end/excel/ExcelData.java
+++ selen/selenium_framework_design/src/test/java/end2end/excel/ExcelData.java
@@ -5,6 +5,8 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Iterator;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
