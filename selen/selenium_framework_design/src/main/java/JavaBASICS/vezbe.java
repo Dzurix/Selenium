5e6bf82ffec5b6241509448b685486bb4d27b42a
Dejan Stanisin <deki2ns@gmail.com>
2022-10-14 06:47:14 +0200

    vezb

--- selen/selenium_framework_design/src/main/java/JavaBASICS/vezbe.java
+++ selen/selenium_framework_design/src/main/java/JavaBASICS/vezbe.java
@@ -15,5 +15,32 @@ public class vezbe {
       }
       System.out.println(""); // printLN ispisuje u sledeci red
     }
+
+    // obrnuto
+
+    int y = 10;
+
+    for (int i = 0; i < 5; i++) {
+      for (int j = 1; j <= i; j++) {
+        System.out.print(y); // print ispisuje sve u jednom redu
+        System.out.print("\t"); //ovako stampamo veliki razmak => kao TAB npr
+        y--; // ovo je za definisanje da li se povecava od y ili se smanjuje
+      }
+      System.out.println(""); // printLN ispisuje u sledeci red
+    }
+
+    //  treci primer
+
+    for (int i = 1; i < 5; i++) {
+      for (int j = 1; j <= i; j++) {
+        System.out.print(j); // print ispisuje sve u jednom redu
+        System.out.print("\t"); //ovako stampamo veliki razmak => kao TAB npr
+      }
+      System.out.println(""); // printLN ispisuje u sledeci red
+    }
   }
+  // INTERFACE je slican CLASSi. Imace metode, ali nema body
+  // Klase treba da definisu i implementuju metode prisutne u INTERFACE
+  // sve varijable definisane u interface su public po prirodi
+  // jedna klasa moze IMPLEMENTS vise INTERFACE
 }
